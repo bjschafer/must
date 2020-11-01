a4734d193fe02f8b1fb5e61e4a95edf1be7d90fa
Braxton Schafer <braxton@epic.com>
2020-10-31 22:59:11 -0500

    get command line parsing sorted out

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,19 @@ pub fn main() {
     let matches = App::new("must")
     .about("Magnetic tape interface in Rust")
     .arg(
+        Arg::new("COMMAND")
+        .about("Which command to execute")
+        .possible_values(&[
+            "erase",
+            "fastforward-filemark",
+            "fastforward-record",
+            "rewind-filemark",
+            "rewind-record",
+            "status",
+        ])
+        .takes_value(true)
+    )
+    .arg(
         Arg::new("device")
         .about("Defines the tape device to operate on")
         .takes_value(true)
@@ -15,17 +28,17 @@ pub fn main() {
         .long("device")
     )
     .arg(
-        Arg::new("COMMAND")
-        .about("Which command to execute")
-        .index(1)
-        .possible_values(&[
-            "status",
-            "fastforward-record",
-            "fastforward-filemark",
-            "rewind-record",
-            "rewind-filemark",
+        Arg::new("count")
+        .about("Determines how many of COMMAND to run")
+        .takes_value(true)
+        .short('c')
+        .long("count")
+        .required_if_eq_any(&[
+            ("COMMAND", "fastforward-record"),
+            ("COMMAND", "fastforward-filemark"),
+            ("COMMAND", "rewind-record"),
+            ("COMMAND", "rewind-filemark"),
         ])
-        .required(true),
     )
     .get_matches();
 
@@ -42,12 +55,14 @@ pub fn main() {
         std::process::exit(66);
     }
 
+    let count: i32 = matches.value_of_t("count").unwrap_or(1);
+
     match matches.value_of("COMMAND").unwrap() {
         "status" => {
             tape::status(device)
         },
         "fastforward-record" | "fastforward-filemark" | "rewind-record" | "rewind-filemark" => {
-            do_long_tape_command(device, matches.value_of("COMMAND").unwrap())
+            do_unary_tape_command(device, matches.value_of("COMMAND").unwrap(), count)
         },
         _ => unreachable!(),
     };
@@ -57,7 +72,7 @@ fn validate_device(device: &str) -> bool {
     Path::new(device).exists()
 }
 
-fn do_long_tape_command(device: &str, command: &str) -> i32 {
+fn do_unary_tape_command(device: &str, command: &str, count: i32) -> i32 {
     let sp = Spinner::new(Spinners::Line, "Executing tape command".into());
     let (move_type, direction) = match command {
         "fastforward-record" => (tape::MovementType::Record, tape::MovementDirection::Forward),
@@ -66,7 +81,7 @@ fn do_long_tape_command(device: &str, command: &str) -> i32 {
         "rewind-filemark" => (tape::MovementType::FileMark, tape::MovementDirection::Backward),
         _ => unreachable!(),
     };
-    let res = tape::move_space(device, move_type, direction, 1);
+    let res = tape::move_space(device, move_type, direction, count);
     sp.stop();
     println!();
     res
